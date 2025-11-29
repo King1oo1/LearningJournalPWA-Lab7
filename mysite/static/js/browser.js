822c78b38d9368305d5ebda343fee05e499348cf
CHANDANDEEP SINGH <pubgk9001@gmail.com>
2025-11-29 22:40:22 +0000

    browser.js

--- mysite/static/js/browser.js
+++ mysite/static/js/browser.js
@@ -1,7 +1,5 @@
 // js/browser.js - Browser API Functions
 
-// js/browser.js - Browser API Functions
-
 // ===== BROWSER API: CLIPBOARD API =====
 function initClipboardAPI() {
     console.log('Initializing Clipboard API...');
